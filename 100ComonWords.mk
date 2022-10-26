d1fb0abe4cfd558f8ee78cbd7b1a8bcf20eb0696
guomengtao <guomengtao@gmail.com>
2022-10-26 05:49:24 -0700

    Create 100ComonWords.mk

---
+++ 100ComonWords.mk
@@ -0,0 +1,50 @@
+ 
+ 
+ ## About me
+
+Hi, I'm Mona. You might recognize me as GitHub's mascot.
+
+| Rank | Languages |
+|-----:|-----------|
+|     1| Javascript|
+|     2| Python    |
+|     3| SQL       |
+
+
+ 
+ 
+## 100 most common words
+ 
+### Parts of speech
+
+The following is a very similar list, subdivided by[part of speech](https://en.wikipedia.org/wiki/Part_of_speech "Part of speech").[\[1\]](https://en.wikipedia.org/wiki/Most_common_words_in_English#cite_note-langfacts-1)The list labeled "Others" includes[pronouns](https://en.wikipedia.org/wiki/Pronoun "Pronoun"),[possessives](https://en.wikipedia.org/wiki/Possessive "Possessive"),[articles](https://en.wikipedia.org/wiki/Article_(grammar) "Article (grammar)"),[modal verbs](https://en.wikipedia.org/wiki/Modal_verb "Modal verb"),[adverbs](https://en.wikipedia.org/wiki/Adverb "Adverb"), and[conjunctions](https://en.wikipedia.org/wiki/Conjunction_(grammar) "Conjunction (grammar)").
+
+| Rank | Nouns | Verbs | Adjectives | Prepositions | Others |
+| --- | --- | --- | --- | --- | --- |
+| 1 | time | be | good | to | the |
+| 2 | person | have | new | of | and |
+| 3 | year | do | first | in | a |
+| 4 | way | say | last | for | that |
+| 5 | day | get | long | on | I |
+| 6 | thing | make | great | with | it |
+| 7 | man | go | little | at | not |
+| 8 | world | know | own | by | he |
+| 9 | life | take | other | from | as |
+| 10 | hand | see | old | up | you |
+| 11 | part | come | right | about | this |
+| 12 | child | think | big | into | but |
+| 13 | eye | look | high | over | his |
+| 14 | woman | want | different | after | they |å
+| 15 | place | give | small |  | her |
+| 16 | work | use | large |  | she |
+| 17 | week | find | next |  | or |
+| 18 | case | tell | early |  | an |
+| 19 | point | ask | young |  | will |
+| 20 | government | work | important |  | my |
+| 21 | company | seem | few |  | one |
+| 22 | number | feel | public |  | all |
+| 23 | group | try | bad |  | would |
+| 24 | problem | leave | same |  | there |
+| 25 | fact | call | able |  | their |
+
+## See also
